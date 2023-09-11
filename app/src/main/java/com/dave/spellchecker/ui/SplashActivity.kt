3e4b7c593a204fad5f8e5bac388fb143c6da2fe1
soulduse <soulduse@naver.com>
2023-09-11 09:53:36 +0900

    update version, add billing init in Splash

--- app/src/main/java/com/dave/spellchecker/ui/SplashActivity.kt
+++ app/src/main/java/com/dave/spellchecker/ui/SplashActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.dave.spellchecker.R
 import com.dave.spellchecker.ui.main.MainActivity
 import com.dave.spellchecker.util.AdProvider
+import com.dave.spellchecker.util.BillingManager
 import com.dave.spellchecker.util.SharedPreferenceProvider
 import com.dave.spellchecker.util.start
 import dagger.hilt.android.AndroidEntryPoint
@@ -12,6 +13,9 @@ import javax.inject.Inject
 
 @AndroidEntryPoint
 class SplashActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var billingManager: BillingManager
 
     @Inject
     lateinit var adProvider: AdProvider
@@ -22,6 +26,7 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        initBillingManager()
         if (!pref.hasSubscribing) {
             initAd()
             return
@@ -35,6 +40,10 @@ class SplashActivity : AppCompatActivity() {
             .dismissCallback { goToMain() }
             .loadInterstitialAd()
             .afterLoaded { adProvider.show() }
+    }
+
+    private fun initBillingManager() {
+        billingManager.startConnection()
     }
 
     private fun goToMain() {
