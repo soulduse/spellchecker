81ba521adb17869fe099be46e94bc31d60eb429a
soulduse <soulduse@naver.com>
2023-09-24 09:09:39 +0900

    유틸성 크래스 추가 및 리소스 추가

---
+++ app/src/main/java/com/dave/spellchecker/util/ViewBindings.kt
@@ -0,0 +1,22 @@
+package com.dave.spellchecker.util
+
+import android.view.View
+import androidx.databinding.BindingAdapter
+
+@BindingAdapter("isGone")
+fun bindIsGone(view: View, isGone: Boolean) {
+    view.visibility = if (isGone) {
+        View.GONE
+    } else {
+        View.VISIBLE
+    }
+}
+
+@BindingAdapter("isInvisible")
+fun bindIsInvisible(view: View, isInvisible: Boolean) {
+    view.visibility = if (isInvisible) {
+        View.INVISIBLE
+    } else {
+        View.VISIBLE
+    }
+}
