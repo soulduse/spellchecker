448982738fb617c3cc58708ff550d10cc471b32e
soulduse <soulduse@naver.com>
2023-10-05 16:13:19 +0900

    리소스 추가

---
+++ app/src/main/java/com/dave/spellchecker/AppData.kt
@@ -0,0 +1,11 @@
+package com.dave.ai
+
+object AppData {
+    const val APP_TITLE = "replace.app_title"
+    const val APP_NAME = "replace.app_name"
+    const val FULL_AD_ID = "replace.full_id"
+    const val BANNER_AD_ID = "replace.banner_id"
+    const val NATIVE_AD_ID = "replace.native_id"
+    const val REWARD_FULL_AD_ID = "replace.reward_full_id"
+    const val PACKAGE_NAME = "replace.package_name"
+}
--- app/src/main/java/com/dave/spellchecker/util/ResultColors.kt
+++ app/src/main/java/com/dave/spellchecker/util/ResultColors.kt
@@ -3,11 +3,10 @@ package com.dave.spellchecker.util
 enum class ResultColors(
     val title: String,
     val beforeHtml: String,
-    val afterHtml: String
+    val afterHtml: String,
 ) {
-    GREEN("띄어쓰기", "<em class='green_text'>", "<font color='#1EC545'>"),
+    RED("맞춤법", "<em class='red_text'>", "<font color='#FC595B'>"),
     VIOLET("표준어의심", "<em class='violet_text'>", "<font color='#B139F4'>"),
+    GREEN("띄어쓰기", "<em class='green_text'>", "<font color='#1EC545'>"),
     BLUE("통계적교정", "<em class='blue_text'>", "<font color='#3AACE7'>"),
-    RED("맞춤법", "<em class='red_text'>", "<font color='#FC595B'>");
-
 }
